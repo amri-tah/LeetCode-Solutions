a48fa015ab03089231c86ed45b734a4d0bcff3d8
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2023-08-19 12:50:19 +0530

    Time: 0 ms (100%), Space: 44.3 MB (6.8%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -3,12 +3,9 @@ class Solution {
         for (int row = 0; row < image.length; row++) {
             int n = image[row].length - 1;
             for (int i = 0; i <= n / 2; i++) {  
-                int temp = image[row][n - i];  
-                image[row][n - i] = image[row][i];  
+                int temp = image[row][n - i]^1;  
+                image[row][n - i] = image[row][i]^1;  
                 image[row][i] = temp;
-            }
-            for (int j = 0; j <= n; j++) {  
-                image[row][j] = (image[row][j] == 0) ? 1 : 0;
             }
         }
         return image;
